37e1c613952acfb752e9c189290a3fa6624764ea
Nico Shinigami <nicokun1316@gmail.com>
2022-05-07 10:15:44 +0200

    make inputs react to current world state (movement in open world, inside combat, UI inputs)

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,32 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour {
     private GameObject player;
 
-    private Rigidbody2D pb;
+    private Rigidbody2D playerBody;
 
-    private MovementController mc;
+    private MovementController playerController;
+
+    private ContactFilter2D interactibleFilter;
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindWithTag("Player");
-        pb = player.GetComponent<Rigidbody2D>();
-        mc = player.GetComponent<MovementController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerBody = player.GetComponent<Rigidbody2D>();
+        playerController = player.GetComponent<MovementController>();
+        interactibleFilter.SetLayerMask(LayerMask.GetMask("Interactible"));
+        interactibleFilter.useLayerMask = true;
     }
 
     public void Move(InputAction.CallbackContext context) {
-        mc.Move(context.ReadValue<Vector2>());
+        var mv = context.ReadValue<Vector2>();
+        switch (GameManager.INSTANCE.currentGameState) {
+            case GameState.UI:
+                break;
+            case GameState.WORLD:
+                playerController.Move(mv);
+                break;
+            case GameState.COMBAT:
+                break;
+            
+        }
     }
 
     public void Confirm(InputAction.CallbackContext context) {
-        //pb.Cast();
+        var currentState = GameManager.INSTANCE.currentGameState;
+        switch (currentState) {
+            case GameState.UI:
+                UIManager.INSTANCE.Interact();
+                break;
+            case GameState.WORLD:
+                UIManager.INSTANCE.Interact();
+                break;
+            case GameState.COMBAT:
+                break;
+        }
     }
 }
