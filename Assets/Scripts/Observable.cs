cb774d2be2a23be873a5804b84f8ad128b1cf0ed
Nico Shinigami <nicokun1316@gmail.com>
2022-05-07 10:13:29 +0200

    revealing text implementation

---
+++ Assets/Scripts/Observable.cs
@@ -0,0 +1,24 @@
+﻿namespace DefaultNamespace {
+    public struct Observable<T> {
+        public Observable(T value) {
+            this.value = value;
+            onChange = null;
+        }
+        private T value;
+        public delegate void ValueChange(T oldValue, T newValue);
+
+        public void set(T newValue) {
+            onChange?.Invoke(value, newValue);
+            value = newValue;
+        }
+
+        public T get() {
+            return value;
+        }
+
+        public static implicit operator Observable<T>(T value) => new(value);
+
+        public event ValueChange onChange;
+
+    }
+}
---
+++ Assets/Scripts/RevealingText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class RevealingText {
+        private readonly String text;
+        public Observable<String> currentText;
+        public bool revealed { get; private set; }
+
+        public RevealingText(String text) {
+            this.text = text;
+            revealed = false;
+            currentText = "";
+        }
+
+        public IEnumerator RevealText() {
+            revealed = false;
+            Debug.Log("Beginning the begin");
+            foreach (var character in text) {
+                Debug.Log($"Adding character `{character}`");
+                currentText.set(currentText.get() + character);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            revealed = true;
+        }
+
+        public void RevealAll() {
+            revealed = true;
+            currentText.set(text);
+        }
+    }
+}
