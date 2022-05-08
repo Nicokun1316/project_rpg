a4107678a5cc1fc870efe956201c3d0a395d4b33
Nico Shinigami <nicokun1316@gmail.com>
2022-05-08 10:59:39 +0200

    Add DialogueText to unity context menu

---
+++ Assets/Scripts/EditorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class EditorExtensions : MonoBehaviour {
+        [MenuItem("GameObject/UI/Dialogue Text", false, 10)]
+        static void CreateDialogueText(MenuCommand command) {
+            var go = new GameObject("DialogueText");
+            go.AddComponent<RectTransform>();
+            go.AddComponent<CanvasRenderer>();
+            var tmp = go.AddComponent<DialogueText>();
+            tmp.fontSize = 16;
+            tmp.text = "Sample text...";
+            GameObjectUtility.SetParentAndAlign(go, command.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(go, $"Create {go.name}");
+            Selection.activeObject = go;
+        }
+    }
+}
