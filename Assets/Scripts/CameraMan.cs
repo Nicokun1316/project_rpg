7cb7aa2f039795e3500552f9aaab61c97dde305a
Nico Shinigami <nicokun1316@gmail.com>
2022-05-21 08:32:29 +0200

    Constrain camera to visible map bounds

--- Assets/Scripts/CameraMan.cs
+++ Assets/Scripts/CameraMan.cs
@@ -1,24 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine.Tilemaps;
 
 public class CameraMan : MonoBehaviour {
     private Rigidbody2D player;
 
-    private Tilemap background;
+    private Vector2 cameraBounds;
+
+    private Bounds sceneBounds;
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
-        background = GameObject.FindWithTag("Background").GetComponent<Tilemap>();
-        var camera = GetComponent<Camera>();
-        
-        var bounds = background.localBounds;
-        //bounds.
+        var pixelCamera = GetComponent<PixelPerfectCamera>();
+        cameraBounds = new Vector2((float) pixelCamera.refResolutionX / pixelCamera.assetsPPU / 2,
+            (float) pixelCamera.refResolutionY / pixelCamera.assetsPPU / 2);
+
+        var background = GameObject.FindWithTag("Background").GetComponent<Tilemap>();
+        sceneBounds = background.localBounds;
     }
 
     // Update is called once per frame
     void Update() {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        var x = Math.Clamp(player.position.x, sceneBounds.min.x + cameraBounds.x, sceneBounds.max.x - cameraBounds.x);
+        var y = Math.Clamp(player.position.y, sceneBounds.min.y + cameraBounds.y, sceneBounds.max.y - cameraBounds.y);
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }
